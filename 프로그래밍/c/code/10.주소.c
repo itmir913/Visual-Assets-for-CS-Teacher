f4f3b89ac8a99e67d916b1025229a56892f9c61b
luminousky913 <itmir913@gmail.com>
2026-09-26 15:48:41 +0900

    프로그래밍 C 10: 배열 [5]를 쓸 때의 뜻을 인덱스로, 초기화하지 않은 포인터의 위험을 구체적으로

--- 프로그래밍/c/code/10.주소.c
+++ 프로그래밍/c/code/10.주소.c
@@ -8,7 +8,7 @@ int main(void) {
     printf("n의 값 %d, n의 자리 %p\n", n, (void *) &n);
     printf("c의 값 %c, c의 자리 %p\n", c, (void *) &c);
     printf("d의 값 %.1f, d의 자리 %p\n", d, (void *) &d);
-    // 찍히는 자리 번호는 «돌릴 때마다 달라진다»
+    // 찍히는 자리 번호는 실행할 때마다 달라질 수 있다
 
     return 0;
 }
